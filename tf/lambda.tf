808702df139aebbee525677700f4b6d44aacd768
Agnes Utomo <agnes.utomo@lendesk.com>
2022-01-31 15:11:43 -0800

    add lambda permission

--- tf/lambda.tf
+++ tf/lambda.tf
@@ -105,29 +105,12 @@ resource "aws_iam_policy" "lambda_use_sns" {
   )
 }
 
-resource "aws_iam_policy" "lambda_allow_sns_invoke" {
-  name = "agnes-${var.project}-${var.environment}-lambda-allow-sns-invoke"
-  policy = jsonencode(
-    {
-      "Version" = "2012-10-17",
-      "Statement" = [
-        {
-          "Action" = [
-            "lambda:InvokeFunction",
-          ],
-          "Effect" = "Allow",
-          "Principal": {
-            "Service": [
-              "sns.amazonaws.com"
-            ]
-          }
-          "Resource" = [
-            "${aws_sns_topic.say_hello.arn}"
-          ]
-        }
-      ]
-    }
-  )
+resource "aws_lambda_permission" "allow_sns" {
+  statement_id  = "AllowExecutionFromSNS"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.hello_world.function_name
+  principal     = "sns.amazonaws.com"
+  source_arn    = aws_sns_topic.say_hello.arn
 }
 
 resource "aws_iam_role_policy_attachment" "lambda_use_cloudwatch" {
